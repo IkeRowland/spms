ad7371bfd1a0789dadc3a034f1f2febb9e60be63
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-04-01 01:37:46 +0300

    Fix mobile responsiveness on login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const LoginPage = () => {
   return (
     <div className='h-screen grid md:grid-cols-2'>
-      <div className='col-span-1 bg-gray-900 text-white flex flex-col items-center justify-center px-8'>
-        <img src="/assets/spms.jpeg" alt="analytics" className="w-60 h-56 rounded-full object-cover" />
-        <h2 className="my-5 text-4xl text-center text-gray-300">Student Perfomance Monitoring System</h2>
-        <p className="text-gray-400">Are you looking for a fast and efficient way to monitor your students perfomance? Worry no more, SPMS software solution got you covered! From upload results to getting parent feedback, all is intergrated together.</p>
+      <div className='col-span-1 bg-gray-900 text-white flex flex-col items-center justify-center p-8'>
+        <img src="/assets/spms.jpeg" alt="analytics" className="w-40 h-40 md:w-60 md:h-56 rounded-full object-cover" />
+        <h2 className="my-3 md:my-5 text-xl md:text-4xl text-center text-gray-300">Student Perfomance Monitoring System</h2>
+        <p className="text-sm md:text-md text-gray-400">Are you looking for a fast and efficient way to monitor your students perfomance? Worry no more, SPMS software solution got you covered! From upload results to getting parent feedback, all is intergrated together.</p>
       </div>
       <div className='col-span-1 flex justify-center items-center'>
-        <form className='w-3/5 h-max flex flex-col flex-wrap p-4'>
+        <form className='md:w-3/5 h-max flex flex-col flex-wrap p-4'>
           <h4 className='my-5 font-semibold text-3xl text-gray-900 capitalize'>
             Welcome back to SPMS
           </h4>

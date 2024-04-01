d66a777ae48828ce3c2619410918f653b1b11761
Matuwamai <wamaimatu@gmail.com>
2024-04-01 18:20:06 +0300

    Revise courses page

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -1,92 +1,96 @@
 const CoursePage = () => {
   return (
-    <div>
-      <h2 className="text-xl font-bold mx-4 ">Courses</h2>
-      <section className="h-screen grid md:grid-cols-5 flex justify-between h-full">
-        <div className="col-span-3 bg-gray-100 px-6px m-2">
-          <h4 className="flex justify-center">2024/2</h4>
-          <table className="w-full border">
-            <thead className="">
-              <tr className="text-left">
-                <th className="border border-gray-300 p-2">ID</th>
-                <th className="border border-gray-300 p-2">Course Code</th>
-                <th className="border border-gray=300 p-2">Course Name</th>
-                <th className="border border-gray-300 p-2">Exam Type</th>
-              </tr>
-            </thead>
-            <tbody className="">
-              <tr>
-                <td className="border border-gray-300 p-2">1</td>
-                <td className="border border-gray-300 p-2">ICS 215</td>
-                <td className="border border-gray-300 p-2">Object Orienred</td>
-                <td className="border border-gray-300 p-2">First Attempt</td>
-              </tr>
-              <tr>
-                <td className="border border-gray-300 p-2">2</td>
-                <td className="border border-gray-300 p-2">ICS 350</td>
-                <td className="border border-gray-300 p-2"> Calculus 1</td>
-                <td className="border border-gray-300 p-2">Supplemtary</td>
-              </tr>{" "}
-              <tr>
-                <td className="border border-gray-300 p-2">3</td>
-                <td className="border border-gray-300 p-2">SMA 128</td>
-                <td className="border border-gray-300 p-2"> Discrete Math</td>
-                <td className="border border-gray-300 p-2">Supplemtary</td>
-              </tr>
-              <tr>
-                <td className="border border-gray-300 p-2">3</td>
-                <td className="border border-gray-300 p-2">ICS 350</td>
-                <td className="border border-gray-300 p-2"> Calculus 1</td>
-                <td className="border border-gray-300 p-2">First Attempt</td>
-              </tr>
-              <tr>
-                <td className="border border-gray-300 p-2">4</td>
-                <td className="border border-gray-300 p-2">ICS 350</td>
-                <td className="border border-gray-300 p-2"> Calculus 1</td>
-                <td className="border border-gray-300 p-2">First Attempt</td>
-              </tr>
-              <tr>
-                <td className="border border-gray-300 p-2">5</td>
-                <td className="border border-gray-300 p-2">ICS 350</td>
-                <td className="border border-gray-300 p-2"> Calculus 1</td>
-                <td className="border border-gray-300 p-2">First Attempt</td>
-              </tr>
-              <tr>
-                <td className="border border-gray-300 p-2">6</td>
-                <td className="border border-gray-300 p-2">ICS 350</td>
-                <td className="border border-gray-300 p-2"> Calculus 1</td>
-                <td className="border border-gray-300 p-2">Supplemtary</td>
-              </tr>
-              <tr>
-                <td className="border border-gray-300 p-2">7</td>
-                <td className="border border-gray-300 p-2">ICS 350</td>
-                <td className="border border-gray-300 p-2"> Calculus 1</td>
-                <td className="border border-gray-300 p-2">First Attempt</td>
-              </tr>
-              <tr>
-                <td className="border border-gray-300 p-2">8</td>
-                <td className="border border-gray-300 p-2">ICS 350</td>
-                <td className="border border-gray-300 p-2"> Calculus 1</td>
-                <td className="border border-gray-300 p-2">First Attempt</td>
-              </tr>
-              <tr>
-                <td className="border border-gray-300 p-2">9</td>
-                <td className="border border-gray-300 p-2">ICS 350</td>
-                <td className="border border-gray-300 p-2"> Calculus 1</td>
-                <td className="border border-gray-300 p-2">First Attempt</td>
-              </tr>
-              <tr>
-                <td className="border border-gray-300 p-2">10</td>
-                <td className="border border-gray-300 p-2">ICS 350</td>
-                <td className="border border-gray-300 p-2"> Calculus 1</td>
-                <td className="border border-gray-300 p-2">First Attempt</td>
-              </tr>
-            </tbody>
-          </table>
+    <div className="bg-gray-100 p-4">
+      <h2 className="text-xl font-bold mb-3">Courses</h2>
+      <section className="grid md:grid-cols-5">
+        <div className="col-span-3 pr-2">
+          <div className="bg-white p-2">
+            <h4 className="text-center font-md-bold">2024/2</h4>
+            <table className="w-full border">
+              <thead className="">
+                <tr className="text-left">
+                  <th className="border border-gray-300 p-2">ID</th>
+                  <th className="border border-gray-300 p-2">Course Code</th>
+                  <th className="border border-gray=300 p-2">Course Name</th>
+                  <th className="border border-gray-300 p-2">Exam Type</th>
+                </tr>
+              </thead>
+              <tbody className="">
+                <tr>
+                  <td className="border border-gray-300 p-2">1</td>
+                  <td className="border border-gray-300 p-2">ICS 215</td>
+                  <td className="border border-gray-300 p-2">
+                    Object Orienred
+                  </td>
+                  <td className="border border-gray-300 p-2">First Attempt</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">2</td>
+                  <td className="border border-gray-300 p-2">ICS 350</td>
+                  <td className="border border-gray-300 p-2"> Calculus 1</td>
+                  <td className="border border-gray-300 p-2">Supplemtary</td>
+                </tr>{" "}
+                <tr>
+                  <td className="border border-gray-300 p-2">3</td>
+                  <td className="border border-gray-300 p-2">SMA 128</td>
+                  <td className="border border-gray-300 p-2"> Discrete Math</td>
+                  <td className="border border-gray-300 p-2">Supplemtary</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">3</td>
+                  <td className="border border-gray-300 p-2">ICS 350</td>
+                  <td className="border border-gray-300 p-2"> Calculus 1</td>
+                  <td className="border border-gray-300 p-2">First Attempt</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">4</td>
+                  <td className="border border-gray-300 p-2">ICS 350</td>
+                  <td className="border border-gray-300 p-2"> Calculus 1</td>
+                  <td className="border border-gray-300 p-2">First Attempt</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">5</td>
+                  <td className="border border-gray-300 p-2">ICS 350</td>
+                  <td className="border border-gray-300 p-2"> Calculus 1</td>
+                  <td className="border border-gray-300 p-2">First Attempt</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">6</td>
+                  <td className="border border-gray-300 p-2">ICS 350</td>
+                  <td className="border border-gray-300 p-2"> Calculus 1</td>
+                  <td className="border border-gray-300 p-2">Supplemtary</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">7</td>
+                  <td className="border border-gray-300 p-2">ICS 350</td>
+                  <td className="border border-gray-300 p-2"> Calculus 1</td>
+                  <td className="border border-gray-300 p-2">First Attempt</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">8</td>
+                  <td className="border border-gray-300 p-2">ICS 350</td>
+                  <td className="border border-gray-300 p-2"> Calculus 1</td>
+                  <td className="border border-gray-300 p-2">First Attempt</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">9</td>
+                  <td className="border border-gray-300 p-2">ICS 350</td>
+                  <td className="border border-gray-300 p-2"> Calculus 1</td>
+                  <td className="border border-gray-300 p-2">First Attempt</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">10</td>
+                  <td className="border border-gray-300 p-2">ICS 350</td>
+                  <td className="border border-gray-300 p-2"> Calculus 1</td>
+                  <td className="border border-gray-300 p-2">First Attempt</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-        <form className="col-span-2 bg-gray-100 h-full my-2">
+        <form className="col-span-2 bg-white p-4 h-max">
           <h2 className="text-xl font-bold p-2"> Register New Course</h2>
-          <table className="w-full border">
+          <table className="border">
             <thead>
               <tr>
                 <th className="border border-gray-300 p-2">Course Code</th>
@@ -96,9 +100,9 @@ const CoursePage = () => {
             <tbody>
               <tr>
                 <td className="border border-gray-300 p-2">
-                  <input placeholder="ICS 215"></input>
+                  <input placeholder="ICS 215" className="w-28"></input>
                 </td>
-                <td>
+                <td className="p-2">
                   <select>
                     <option>First Attempt</option>
                     <option>Suplement</option>
@@ -107,9 +111,9 @@ const CoursePage = () => {
               </tr>
               <tr>
                 <td className="border border-gray-300 p-2">
-                  <input placeholder="ICS 215"></input>
+                  <input placeholder="ICS 215" className="w-28"></input>
                 </td>
-                <td>
+                <td className="p-2">
                   <select>
                     <option>First Attempt</option>
                     <option>Suplement</option>
@@ -118,9 +122,9 @@ const CoursePage = () => {
               </tr>
               <tr>
                 <td className="border border-gray-300 p-2">
-                  <input placeholder="ICS 215"></input>
+                  <input placeholder="ICS 215" className="w-28"></input>
                 </td>
-                <td>
+                <td className="p-2">
                   <select>
                     <option>First Attempt</option>
                     <option>Suplement</option>
@@ -129,9 +133,9 @@ const CoursePage = () => {
               </tr>
               <tr>
                 <td className="border border-gray-300 p-2">
-                  <input placeholder="ICS 215"></input>
+                  <input placeholder="ICS 215" className="w-28"></input>
                 </td>
-                <td>
+                <td className="p-2">
                   <select>
                     <option>First Attempt</option>
                     <option>Suplement</option>
@@ -140,9 +144,9 @@ const CoursePage = () => {
               </tr>
               <tr>
                 <td className="border border-gray-300 p-2">
-                  <input placeholder="ICS 215"></input>
+                  <input placeholder="ICS 215" className="w-28"></input>
                 </td>
-                <td>
+                <td className="p-2">
                   <select>
                     <option>First Attempt</option>
                     <option>Suplement</option>

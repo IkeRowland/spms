a3842deb784d36d9f6fa8f30eb3d436bb23fef78
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-04-02 15:49:37 +0300

    Fix creating student from customuser

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import add_students
+from .views import add_students, delete_user
 
 urlpatterns = [
-    path('users/add/students/', add_students)
+    path('users/add/students/', add_students),
+    path('users/<int:user_id>/delete/', delete_user),
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import StudentSerializer, CustomUserSerializer
+from .models import CustomUser
 
 
 @api_view(['POST'])    
@@ -42,3 +43,16 @@ def add_students(request):
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
         return Response(usersList, status=status.HTTP_201_CREATED)
+
+
+@api_view(['DELETE'])
+def delete_user(request, user_id):
+    if request.method == 'DELETE':
+        try:
+            user = CustomUser.objects.get(id=user_id)
+
+            user.delete()
+            return Response({"message": "User deleted successfully!"}, status=status.HTTP_204_NO_CONTENT)
+        except CustomUser.DoesNotExist:
+            return Response({"message": "Not found!"}, status=status.HTTP_404_NOT_FOUND)
+        
